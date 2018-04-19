cfb185088a26d9223991c198d40341b355145890
Guilherme Gall <gmgall@gmail.com>
2018-04-19 10:33:10 -0300

    Converte de Python 2.7 para Python 3

--- modelr.py
+++ modelr.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib
-import urllib2
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
 import json
 import csv
 
 def get_experiments_by_status(status=2):
     url = 'http://model-r.jbrj.gov.br/ws/?status=' + str(status)
-    response = urllib2.urlopen(url)
-    all_exp_list = json.loads(response.read())
+    response = urllib.request.urlopen(url)
+    all_exp_list = json.loads(response.read().decode('utf-8'))
     return all_exp_list
 
 def update_experiment_status(id_experiment, status):
     url = 'https://model-r.jbrj.gov.br/ws/setstatus.php?id=%s&status=%d' %(id_experiment, int(status))
-    response = urllib2.urlopen(url)
-    update_status = json.loads(response.read())
+    response = urllib.request.urlopen(url)
+    update_status = json.loads(response.read().decode('utf-8'))
     return update_status['experiment'][0]
 
 def get_occurrences_by_status(experiment, status):
@@ -33,11 +33,11 @@ def write_occurrences_csv(list_of_points, output_file):
 # TODO raise Exception when a web service error occurs.
 # Right now it only prints the response for debugging purposes.
 def inform_experiment_results(params):
-    query = urllib.urlencode(params)
+    query = urllib.parse.urlencode(params).encode('utf-8')
     url = 'https://model-r.jbrj.gov.br/ws/setresult.php'
-    f = urllib.urlopen(url, query)
+    f = urllib.request.urlopen(url, query)
     contents = f.read()
-    print contents
+    print(contents)
     f.close()
 
 def inform_partitions(exp_id, partition, algorithm, kappa, spec_sens,
